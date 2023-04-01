e5d0e315a580c402c5327bbf9d3570036eede4ec
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-04-01 19:43:10 +0800

    add causal analysis

--- causal_analysis.py
+++ causal_analysis.py
@@ -13,69 +13,6 @@ from utils import *
 import torch.nn.functional as F
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser(description='Conditional Adversarial Generator')
-parser.add_argument('--data_dir', default='data/ImageNet1k', help='ImageNet Validation Data')
-parser.add_argument('--test_dir', default='', help='Testing Data')
-parser.add_argument('--result_dir', default='', help='Result output')
-parser.add_argument('--batch_size', type=int, default=10, help='Batch Size')
-parser.add_argument('--model_t', type=str, default='res152', help='Model under attack : vgg16, vgg19, dense121')
-parser.add_argument('--layer', type=int, default=1, help='layer')
-args = parser.parse_args()
-print(args)
-
-# Normalize (0-1)
-
-n_class = 1000
-# GPU
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-# Input dimensions: Inception takes 3x299x299
-if args.model_t in ['incv3', 'incv4']:
-    img_size = 299
-else:
-    img_size = 224
-
-model_t = load_model(args)
-model_t = nn.DataParallel(model_t).cuda()
-model_t.eval()
-
-# Setup-Data
-data_transform = transforms.Compose([
-    transforms.Resize(img_size),
-    transforms.ToTensor(),
-])
-
-mean = [0.485, 0.456, 0.406]
-std = [0.229, 0.224, 0.225]
-
-
-def normalize(t):
-    t[:, 0, :, :] = (t[:, 0, :, :] - mean[0]) / std[0]
-    t[:, 1, :, :] = (t[:, 1, :, :] - mean[1]) / std[1]
-    t[:, 2, :, :] = (t[:, 2, :, :] - mean[2]) / std[2]
-    return t
-
-
-class_ids = np.array([150, 507, 62, 843, 426, 590, 715, 952])
-
-# Evaluation
-sr = np.zeros(len(class_ids))
-for idx in range(len(class_ids)):
-    test_dir = '{}_t{}'.format(args.test_dir, class_ids[idx])
-    test_set = datasets.ImageFolder(test_dir, data_transform)
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=4,
-                                              pin_memory=True)
-    neuron_ranking = solve_causal(test_loader, model_t, args.model_t, class_ids[idx], 1000, split_layer=43, use_cuda=True)
-    # find outstanding neuron neuron_ranking shape: 4096x2
-    temp = neuron_ranking
-    ind = np.argsort(temp[:, 1])[::-1]
-    temp = temp[ind]
-    top = outlier_detection(temp[:, 1], max(temp[:, 1]), verbose=False)
-    outstanding_neuron = temp[0: len(top)][:, 0]
-
-    print('total:{}, top:{}'.format(len(neuron_ranking), len(outstanding_neuron)))
-    np.save(os.path.join(args.result_dir, str(model_t) + '_outstanding.npy'), outstanding_neuron)
-        
 
 def solve_causal(data_loader, model, arch, target_class, num_sample, split_layer=43, use_cuda=True):
     #split the model
@@ -218,3 +155,67 @@ class Flatten(nn.Module):
     def forward(self, x):
         x = x.view(x.size(0), -1)
         return x
+
+
+parser = argparse.ArgumentParser(description='Conditional Adversarial Generator')
+parser.add_argument('--data_dir', default='data/ImageNet1k', help='ImageNet Validation Data')
+parser.add_argument('--test_dir', default='', help='Testing Data')
+parser.add_argument('--result_dir', default='', help='Result output')
+parser.add_argument('--batch_size', type=int, default=10, help='Batch Size')
+parser.add_argument('--model_t', type=str, default='res152', help='Model under attack : vgg16, vgg19, dense121')
+parser.add_argument('--layer', type=int, default=1, help='layer')
+args = parser.parse_args()
+print(args)
+
+# Normalize (0-1)
+
+n_class = 1000
+# GPU
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# Input dimensions: Inception takes 3x299x299
+if args.model_t in ['incv3', 'incv4']:
+    img_size = 299
+else:
+    img_size = 224
+
+model_t = load_model(args)
+model_t = nn.DataParallel(model_t).cuda()
+model_t.eval()
+
+# Setup-Data
+data_transform = transforms.Compose([
+    transforms.Resize(img_size),
+    transforms.ToTensor(),
+])
+
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+
+def normalize(t):
+    t[:, 0, :, :] = (t[:, 0, :, :] - mean[0]) / std[0]
+    t[:, 1, :, :] = (t[:, 1, :, :] - mean[1]) / std[1]
+    t[:, 2, :, :] = (t[:, 2, :, :] - mean[2]) / std[2]
+    return t
+
+
+class_ids = np.array([150, 507, 62, 843, 426, 590, 715, 952])
+
+# Evaluation
+sr = np.zeros(len(class_ids))
+for idx in range(len(class_ids)):
+    test_dir = '{}_t{}'.format(args.test_dir, class_ids[idx])
+    test_set = datasets.ImageFolder(test_dir, data_transform)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=4,
+                                              pin_memory=True)
+    neuron_ranking = solve_causal(test_loader, model_t, args.model_t, class_ids[idx], 1000, split_layer=43, use_cuda=True)
+    # find outstanding neuron neuron_ranking shape: 4096x2
+    temp = neuron_ranking
+    ind = np.argsort(temp[:, 1])[::-1]
+    temp = temp[ind]
+    top = outlier_detection(temp[:, 1], max(temp[:, 1]), verbose=False)
+    outstanding_neuron = temp[0: len(top)][:, 0]
+
+    print('total:{}, top:{}'.format(len(neuron_ranking), len(outstanding_neuron)))
+    np.save(os.path.join(args.result_dir, str(model_t) + '_outstanding.npy'), outstanding_neuron)
