a686180b5b29d1d32e7f72c148c7b10c9f1e3f8a
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-04-05 16:14:42 +0800

    add causal analysis

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python causal_analysis.py --option=causal --data_dir=data/ImageNet1k/ --test_dir ./results/gan_n8/model-inv3-epoch9 --result_dir=./output/ --model_t=vgg19 --layer=43
-python causal_analysis.py --option=act --data_dir=data/ImageNet1k/ --test_dir ./results/gan_n8/model-inv3-epoch9 --result_dir=./output/ --model_t=vgg19 --layer=43
+python causal_analysis.py --option=causal --test_type=npy --test_dir=./output --result_dir=./output/ --model_t=vgg19 --model_t_ae=dense201 --layer=43
+python causal_analysis.py --option=act --test_type=npy --test_dir=./output --result_dir=./output/ --model_t=vgg19 --model_t_ae=dense201 --layer=43
 
 python causal_analysis.py --option=plot --result_dir=./output/ --model_t=vgg19
